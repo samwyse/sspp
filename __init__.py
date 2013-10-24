af3b8ddbbde415eab85dd4fb270d1a012c0790b7
Sam Denton <samwyse@gmail.com>
2013-10-23 22:07:36 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from glob import glob
+from keyword import iskeyword
 from os.path import dirname, join, split, splitext
 
 basedir = dirname(__file__)
@@ -6,7 +7,13 @@ basedir = dirname(__file__)
 __all__ = []
 for name in glob(join(basedir, '*.py')):
     module = splitext(split(name)[-1])[0]
-    if not module.startswith('_') and module.isidentifier():
-        __import__(__name__+'.'+module)
-        __all__.append(module)
+    if not module.startswith('_') and module.isidentifier() and not iskeyword(module):
+        try:
+            __import__(__name__+'.'+module)
+        except:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.warning('Ignoring exception while loading the %r plug-in.', module)
+        else:
+            __all__.append(module)
 __all__.sort()
